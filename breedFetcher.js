0bef6a22cb956eecfbde1b003016c5d8296ad5a7
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-05 19:53:24 +0000

    testing functionality implemented

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,14 +2,16 @@ const request = require('request');
 
 const fetchBreedDescription = function(breedName, callBack) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-  request(url, (error, status, body) => {
-    if (error) {
-      callBack(error, null);
+  request(url, (err, status, body) => {
+    const data = JSON.parse(body);
+    if (err || data.length === 0) {
+      callBack(err, null);
     } else {
-      const data = JSON.parse(body);
-      callBack(null, data);
+      callBack(null, data[0]["description"]);
     }
   });
 };
+
+
 
 module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const breedName = process.argv[2];
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
-  } else {
-    if (desc.length > 0) {
-      console.log(desc);
-    } else console.log("Ooops! Your breed could not be found!");
-  }
+  } else console.log(desc);
+    
 });
